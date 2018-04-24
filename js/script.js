86a4704c0b6686c141848b3f54d1ce6a94b9b35c
Cornelia <cornelia.schulz.nz@gmail.com>
2018-04-24 23:58:23 +1200

    added HTML and CSS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+Get input by determining which button was pressed
+display the input on the screen
+Store input
+When = is pressed, compute the calculation
+Display the result on the screen
+When AC is pressed clear everything
+When CE is pressed clear the last input
+*/
